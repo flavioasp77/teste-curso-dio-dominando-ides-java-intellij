1d0ea2a9d8723aa86576c8ad6e10b3c8a3a6dd23
flavioasp77 <flavioasp77@gmail.com>
2022-07-22 16:03:49 -0300

    criada a class Livro

--- src/br/com/curso/PrimeiroPrograma.java
+++ src/br/com/curso/PrimeiroPrograma.java
@@ -2,12 +2,62 @@ package br.com.curso;
 
 import br.com.curso.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-/*        int a = 5;
-        int b = 3;
-        System.out.println("Hello World! " + (a + b));*/
+
         Gato gato = new Gato(null, null, null);
         System.out.println(gato);
+
+        Livro livro = new Livro("O Robbit", 300);
+
+    }
+}
+
+class Livro {
+    private String titulo;
+    private Integer numeroPaginas;
+
+    public Livro(String titulo, Integer numeroPaginas) {
+        this.titulo = titulo;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(titulo, livro.titulo) && Objects.equals(numeroPaginas, livro.numeroPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, numeroPaginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
     }
 }
